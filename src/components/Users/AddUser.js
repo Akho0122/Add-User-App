035d44370dad33ac8028bd248d1a25ecf08980bf
Akho0122 <mr.akhror.dev@gmail.com>
2022-01-21 19:46:25 +0500

    Error Modal was created

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
@@ -28,6 +29,8 @@ const AddUser = (props) => {
   };
 
   return (
+    <>
+    <ErrorModal title="An error occured!" message="something went wrong!"/>
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
@@ -47,6 +50,7 @@ const AddUser = (props) => {
         <Button type="submit">Add User</Button>
       </form>
     </Card>
+    </>
   );
 };
 
